713bfa02c5cd9e751c8551473a08c4b25807941f
devinxl <devin@nodereal.io>
2023-06-12 16:30:41 +0800

    feat(dcellar-web-ui): opt script, fonts, packages and prelink

--- apps/dcellar-web-ui/src/modules/file/utils/genCreateObjectTx.ts
+++ apps/dcellar-web-ui/src/modules/file/utils/genCreateObjectTx.ts
@@ -1,5 +1,4 @@
 import { client } from "@/base/client";
-import { GET_APPROVAL_INTERVAL } from "@/constants/common";
 import { TCreateObject } from "@bnb-chain/greenfield-chain-sdk";
 
 export const genCreateObjectTx = async (configParam: TCreateObject) => {
--- apps/dcellar-web-ui/src/pages/_document.tsx
+++ apps/dcellar-web-ui/src/pages/_document.tsx
@@ -2,6 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 import { Ga4 } from '@/components/common/Ga4';
 import { GA_ID, assetPrefix } from '@/base/env';
+import Script from 'next/script';
+import { LCP_IMAGES } from '@/constants/common';
 
 export default function Document() {
   return (
@@ -11,15 +13,21 @@ export default function Document() {
           name="google-site-verification"
           content="9P1xIkjIzkjS3UiiTBjcjN5tfyh4Yk6FDKELgtTdMGE"
         />
+        <link rel="preconnect" href={`${assetPrefix}`} />
         <link rel="icon preload" href={`${assetPrefix}/favicon.svg`} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <>
+          {LCP_IMAGES.map((url: string) => {
+            <link rel="preload" href={url} as="image" />;
+          })}
+        </>
         <link
+          rel="stylesheet preload"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600&display=swap"
-          rel="stylesheet"
         />
-        <script defer src={`${assetPrefix}/zk-wasm/wasm_exec.js`}></script>
-        <script defer src={`${assetPrefix}/zk-wasm/init.js`}></script>
+        <Script src={`${assetPrefix}/zk-wasm/wasm_exec.js`} strategy="afterInteractive"></Script>
+        <Script src={`${assetPrefix}/zk-wasm/init.js`} strategy="afterInteractive"></Script>
         <Ga4 id={GA_ID} />
       </Head>
       <body style={{ background: 'transparent' }}>
