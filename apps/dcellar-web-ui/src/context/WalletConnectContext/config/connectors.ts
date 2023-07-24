07c42789a33d9e3c1c15f8331ee90b04aba3bd73
Wenty Li <105278450+wenty22@users.noreply.github.com>
2023-07-24 17:31:58 +0800

    fix(dcellar-web-ui): fix refresh page will logout if trust wallet isn't default wallet (#150)
    * fix(dcellar-web-ui): force use trust wallet if trust wallet is set as the default wallet
    
    * fix(dcellar-web-ui): add window.ethereum if window.trustwallet is undefined
    
    * fix(dcellar-web-ui): fix refresh page will logout if trust wallet isn't default wallet
    
    * fix(dcellar-web-ui): fix typo
    
    * fix(dcellar-web-ui): fix build error
    
    * test(dcellar-web-ui): add log
    
    * fix(dcellar-web-ui): add log
    
    * style: format code

--- apps/dcellar-web-ui/src/context/WalletConnectContext/config/connectors.ts
+++ apps/dcellar-web-ui/src/context/WalletConnectContext/config/connectors.ts
@@ -1,41 +1,37 @@
 import { chains } from '@/context/WalletConnectContext/config/chains';
 import { InjectedConnector } from 'wagmi/connectors/injected';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 
-const metaMaskConnector = new InjectedConnector({
-  chains,
-  options: {
-    name: 'MetaMask',
-    shimDisconnect: true,
-    getProvider: () => {
-      if (typeof window === 'undefined') {
-        return;
-      }
-      // If trust wallet is already set as the default wallet, force using trustwallet
-      if (window.ethereum?.isTrustWallet) {
-        return window.trustwallet ?? window.ethereum
-      }
-      return window.ethereum
-    }
-  },
-});
+function getMetaMaskConnector() {
+  // If trust wallet is already set as the default wallet, force using trustwallet
+  if (typeof window !== 'undefined' && window.ethereum?.isTrustWallet) {
+    return new InjectedConnector({
+      chains,
+      options: {
+        name: 'MetaMask',
+        shimDisconnect: true,
+        getProvider: () => {
+          if (typeof window === 'undefined') {
+            return;
+          }
+          return window.trustwallet ?? window.ethereum
+        }
+      },
+    });
+  }
+
+  return new MetaMaskConnector({ chains })
+}
 
 const trustWalletConnector = new InjectedConnector({
   chains,
   options: {
     name: 'Trust Wallet',
     shimDisconnect: true,
-    getProvider: () => {
-      if (typeof window === 'undefined') {
-        return;
-      }
-      if (window.trustwallet) {
-        Object.defineProperty(window.trustwallet, 'removeListener', {
-          value: window.trustwallet.off,
-        });
-      }
-      return window.trustwallet
-    }
+    getProvider: () => (typeof window !== 'undefined' ? window.trustwallet : undefined),
   },
 });
 
-export const connectors = [trustWalletConnector, metaMaskConnector];
+const metaMaskConnector = getMetaMaskConnector()
+
+export const connectors = [trustWalletConnector, metaMaskConnector] as InjectedConnector[];
