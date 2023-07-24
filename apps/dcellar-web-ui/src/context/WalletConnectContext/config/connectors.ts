3f96dcdbb1e8091dfecad6001251b9ae40b1aa43
Wenty Li <105278450+wenty22@users.noreply.github.com>
2023-07-24 14:31:18 +0800

    fix(dcellar-web-ui): force use trust wallet if trust wallet is set as… (#148)
    * fix(dcellar-web-ui): force use trust wallet if trust wallet is set as the default wallet
    
    * fix(dcellar-web-ui): add window.ethereum if window.trustwallet is undefined

--- apps/dcellar-web-ui/src/context/WalletConnectContext/config/connectors.ts
+++ apps/dcellar-web-ui/src/context/WalletConnectContext/config/connectors.ts
@@ -1,16 +1,41 @@
 import { chains } from '@/context/WalletConnectContext/config/chains';
 import { InjectedConnector } from 'wagmi/connectors/injected';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+
+const metaMaskConnector = new InjectedConnector({
+  chains,
+  options: {
+    name: 'MetaMask',
+    shimDisconnect: true,
+    getProvider: () => {
+      if (typeof window === 'undefined') {
+        return;
+      }
+      // If trust wallet is already set as the default wallet, force using trustwallet
+      if (window.ethereum?.isTrustWallet) {
+        return window.trustwallet ?? window.ethereum
+      }
+      return window.ethereum
+    }
+  },
+});
 
 const trustWalletConnector = new InjectedConnector({
   chains,
   options: {
     name: 'Trust Wallet',
     shimDisconnect: true,
-    getProvider: () => (typeof window !== 'undefined' ? window.trustwallet : undefined),
+    getProvider: () => {
+      if (typeof window === 'undefined') {
+        return;
+      }
+      if (window.trustwallet) {
+        Object.defineProperty(window.trustwallet, 'removeListener', {
+          value: window.trustwallet.off,
+        });
+      }
+      return window.trustwallet
+    }
   },
 });
 
-const metalMaskConnector = new MetaMaskConnector({ chains });
-
-export const connectors = [trustWalletConnector, metalMaskConnector];
+export const connectors = [trustWalletConnector, metaMaskConnector];
