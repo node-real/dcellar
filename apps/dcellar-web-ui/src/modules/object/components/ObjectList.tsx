34e924c607dc74989cc86873b0eff1532afceb15
Miyaww <95063753+Miyaww@users.noreply.github.com>
2023-07-26 18:58:13 +0800

    fix(dcellar-web-ui): fix modal popup issue (#156)

--- apps/dcellar-web-ui/src/modules/object/components/ObjectList.tsx
+++ apps/dcellar-web-ui/src/modules/object/components/ObjectList.tsx
@@ -358,7 +358,7 @@ export const ObjectList = memo<ObjectListProps>(function ObjectList() {
   return (
     <>
       {editCreate && <CreateFolder refetch={refetch} />}
-      {editDelete?.objectName && <DeleteObject refetch={refetch} />}
+      {editDelete?.objectName && !deleteFolderNotEmpty &&<DeleteObject refetch={refetch} />}
       {deleteFolderNotEmpty && <FolderNotEmpty />}
       {statusDetail.title && <StatusDetail />}
       {editDetail?.objectName && <DetailObject />}
