b5342d9af9e765950213a19c8c2622da4aac6f63
Wenty Li <wenty.l@nodereal.io>
2023-08-07 23:57:48 +0800

    test: add log

--- apps/dcellar-web-ui/src/context/WalletConnectContext/hooks/useWalletSwitchAccount.ts
+++ apps/dcellar-web-ui/src/context/WalletConnectContext/hooks/useWalletSwitchAccount.ts
@@ -11,6 +11,8 @@ export function useWalletSwitchAccount(handler: WalletSwitchAccountHandler) {
 
   useEffect(() => {
     const onChange = (data: ConnectorData) => {
+      console.log('useWalletSwitchAccount change', 'address: ', address, 'data: ', data)
+
       if (data.account && data.account !== address) {
         handlerRef.current?.(data);
       }
