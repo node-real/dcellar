237bb813eb423eb9f784fad29bdb5aea99dcc476
Wenty Li <wenty.l@nodereal.io>
2023-08-07 22:01:49 +0800

    refactor: fix wallet issue

--- apps/dcellar-web-ui/src/context/WalletConnectContext/connectors/TrustWalletConnector.ts
+++ apps/dcellar-web-ui/src/context/WalletConnectContext/connectors/TrustWalletConnector.ts
@@ -1,7 +1,5 @@
-import { Chain, ConnectorNotFoundError, ResourceUnavailableError, RpcError, UserRejectedRequestError } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { Address, getClient } from '@wagmi/core'
-import { getAddress } from '@ethersproject/address'
+import { Chain } from 'wagmi'
+import { MetaMaskConnector as WagmiMetaMaskConnector  } from 'wagmi/connectors/metaMask';
 
 export function getTrustWalletProvider() {
   try {
@@ -24,8 +22,8 @@ export type TrustWalletConnectorOptions = {
 }
 
 
-export class TrustWalletConnector extends InjectedConnector {
-  readonly id = 'trustWallet'
+export class TrustWalletConnector extends WagmiMetaMaskConnector {
+  readonly id: any = 'trustWallet'
 
   constructor({
     chains,
@@ -46,81 +44,5 @@ export class TrustWalletConnector extends InjectedConnector {
       chains,
       options,
     })
-  }
-
-  private handleFailedConnect(error: Error): never {
-    if (this.isUserRejectedRequestError(error)) {
-      throw new UserRejectedRequestError(error)
-    }
-
-    if ((error as RpcError).code === -32002) {
-      throw new ResourceUnavailableError(error)
-    }
-
-    throw error
-  }
-
-  async connect({ chainId }: { chainId?: number } = {}) {
-    try {
-      const provider = await this.getProvider()
-
-      if (!provider) {
-        throw new ConnectorNotFoundError()
-      }
-
-      if (provider.on) {
-        provider.on('accountsChanged', this.onAccountsChanged)
-        provider.on('chainChanged', this.onChainChanged)
-        provider.on('disconnect', this.onDisconnect)
-      }
-
-      this.emit('message', { type: 'connecting' })
-
-      let account: Address | null = null
-      if (this.options?.shimDisconnect && !getClient().storage?.getItem(this.shimDisconnectKey)) {
-        account = await this.getAccount().catch(() => null)
-        const isConnected = !!account
-        if (isConnected) {
-          try {
-            await provider.request({
-              method: 'wallet_requestPermissions',
-              params: [{ eth_accounts: {} }],
-            })
-            account = await this.getAccount()
-          } catch (error) {
-            if (this.isUserRejectedRequestError(error)) {
-              throw new UserRejectedRequestError(error)
-            }
-          }
-        }
-      }
-
-      if (!account) {
-        const accounts = await provider.request({
-          method: 'eth_requestAccounts',
-        })
-        account = getAddress(accounts[0] as string)
-      }
-
-      let id = await this.getChainId()
-      let unsupported = this.isChainUnsupported(id)
-      if (chainId && id !== chainId) {
-        const chain = await this.switchChain(chainId)
-        id = chain.id
-        unsupported = this.isChainUnsupported(id)
-      }
-
-      if (this.options?.shimDisconnect) {
-        getClient().storage?.setItem(this.shimDisconnectKey, true)
-      }
-
-      return { account, chain: { id, unsupported }, provider }
-    } catch (error) {
-      this.handleFailedConnect(error as Error)
-    }
-  }
-
-  async getProvider() {
-    return getTrustWalletProvider()
   }
 }
