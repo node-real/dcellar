434a38b1d844c44e9a1275fcac9a15d6a0411ba7
aidencao <aiden.c@nodereal.io>
2023-09-01 15:05:16 +0800

    feat(dcellar-web-ui): fix quotaComsumedSize

--- apps/dcellar-web-ui/src/modules/bucket/components/DetailDrawer.tsx
+++ apps/dcellar-web-ui/src/modules/bucket/components/DetailDrawer.tsx
@@ -160,8 +160,8 @@ export const DetailDrawer = memo<DetailDrawerProps>(function DetailDrawer() {
 
   const transformedRemainingQuota = useMemo(() => {
     if (!quota) return '--';
-    const { freeQuota, readQuota, consumedQuota } = quota;
-    const remainingQuota = readQuota + freeQuota - consumedQuota;
+    const { freeQuota, readQuota, consumedQuota, freeConsumedSize } = quota;
+    const remainingQuota = readQuota + freeQuota - consumedQuota - freeConsumedSize;
     return `${formatBytes(remainingQuota, true)} / ${formatBytes(freeQuota + readQuota)}`;
   }, [quota]);
 
