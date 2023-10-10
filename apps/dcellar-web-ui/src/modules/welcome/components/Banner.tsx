da0db017a2a8e75e347b78ebdedd0f5ae1eee33e
devinxl <devin@nodereal.io>
2023-10-10 14:54:22 +0800

    feat(dcellar-web-ui): update welcome page banner style

--- apps/dcellar-web-ui/src/modules/welcome/components/Banner.tsx
+++ apps/dcellar-web-ui/src/modules/welcome/components/Banner.tsx
@@ -1,5 +1,5 @@
 import { assetPrefix } from '@/base/env';
-import { Flex, Image, Text } from '@totejs/uikit';
+import { Box, Flex, Image, Text } from '@totejs/uikit';
 import { ConnectWallet } from '@/components/ConnectWallet';
 import { smMedia } from '@/modules/responsive';
 import { LandingResponsiveContainer, lgMedia, mdMedia } from '..';
@@ -7,86 +7,95 @@ import { INTER_FONT } from '@/modules/wallet/constants';
 
 export const Banner = () => {
   return (
-    <LandingResponsiveContainer bg={`url(${assetPrefix}/images/welcome/bg.png) no-repeat top/auto 100%`}>
-      <Flex paddingTop={144}  sx={{
-        [smMedia]: {
-          flexDirection: 'column',
-          pt: 104
-        }
-      }}>
+    <Box bg={`url(${assetPrefix}/images/welcome/bg.png) no-repeat top/auto 100%`}>
+      <LandingResponsiveContainer>
         <Flex
-          flexDir="column"
-          alignItems="flex-start"
-          flex={1}
-          justifyContent={'flex-end'}
-          alignContent={'flex-end'}
-          marginRight={40}
+          paddingTop={144}
           sx={{
-            [mdMedia]: {
-              marginRight: 16,
+            [smMedia]: {
+              flexDirection: 'column',
+              pt: 104,
             },
           }}
         >
-          <Text
-            as={'h1'}
-            fontSize={48}
-            fontWeight={700}
-            fontFamily={INTER_FONT}
+          <Flex
+            flexDir="column"
+            alignItems="flex-start"
+            flex={1}
+            justifyContent={'flex-end'}
+            alignContent={'flex-end'}
+            marginRight={40}
             sx={{
-              [lgMedia]: {
-                fontSize: '32px',
-              },
               [mdMedia]: {
-                fontSize: '24px',
-              },
-              [smMedia]: {
-                fontSize: '24px',
+                marginRight: 16,
               },
             }}
           >
-            Storage Console for Developers on BNB Greenfield Network
-          </Text>
-          <Text
-            as="h2"
-            my={20}
-            fontSize={20}
-            fontWeight={400}
-            fontFamily={INTER_FONT}
-            color={'readable.secondary'}
-            sx={{
-              [lgMedia]: {
-                fontSize: '20px',
-              },
-              [mdMedia]: {
-                fontSize: '16px',
-              },
-              [smMedia]: {
-                fontSize: '14px',
-              },
-            }}
-          >
-            Empower developers to build with BNB Greenfield Network at ease, assist in development
-            process and team collaboration.
-          </Text>
-          <ConnectWallet gaClickName="dc_lp.homepage.hero.get_started.click" text="Get Started" marginBottom={40}/>
+            <Text
+              as={'h1'}
+              fontSize={48}
+              fontWeight={700}
+              fontFamily={INTER_FONT}
+              sx={{
+                [lgMedia]: {
+                  fontSize: '32px',
+                },
+                [mdMedia]: {
+                  fontSize: '24px',
+                },
+                [smMedia]: {
+                  fontSize: '24px',
+                },
+              }}
+            >
+              Storage Console for Developers on BNB Greenfield Network
+            </Text>
+            <Text
+              as="h2"
+              my={20}
+              fontSize={20}
+              fontWeight={400}
+              fontFamily={INTER_FONT}
+              color={'readable.secondary'}
+              sx={{
+                [lgMedia]: {
+                  fontSize: '20px',
+                },
+                [mdMedia]: {
+                  fontSize: '16px',
+                },
+                [smMedia]: {
+                  fontSize: '14px',
+                },
+              }}
+            >
+              Empower developers to build with BNB Greenfield Network at ease, assist in development
+              process and team collaboration.
+            </Text>
+            <ConnectWallet
+              gaClickName="dc_lp.homepage.hero.get_started.click"
+              text="Get Started"
+              marginBottom={40}
+            />
+          </Flex>
+          <Flex justifySelf={'flex-end'} alignSelf={'flex-end'}>
+            <Image
+              alt="dcellar function screenshot"
+              src={`${assetPrefix}/images/welcome/banner-1.png`}
+              w={553}
+              sx={{
+                [mdMedia]: {
+                  w: '440px',
+                },
+                [smMedia]: {
+                  w: '100%',
+                  marginTop: '24px',
+                },
+              }}
+            />
+          </Flex>
         </Flex>
-        <Flex justifySelf={'flex-end'} alignSelf={'flex-end'}>
-          <Image
-            alt="dcellar function screenshot"
-            src={`${assetPrefix}/images/welcome/banner.png`}
-            w={553}
-            sx={{
-              [mdMedia]: {
-                w: '440px',
-              },
-              [smMedia]: {
-                w: '100%',
-                marginTop: '24px',
-              },
-            }}
-          />
-        </Flex>
-      </Flex>
-    </LandingResponsiveContainer>
+      </LandingResponsiveContainer>
+    </Box>
   );
 };
