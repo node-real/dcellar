fc6be875f4e221ef6edbb2ca057c00e847f1b318
devinxl <devin@nodereal.io>
2023-10-30 21:59:38 +0800

    fix(dcellar-web-ui): remove test codes

--- apps/dcellar-web-ui/src/components/layout/Header/index.tsx
+++ apps/dcellar-web-ui/src/components/layout/Header/index.tsx
@@ -8,14 +8,12 @@ import { SelectNetwork } from '../Common/SelectNetwork';
 import { CookiePolicyContainer } from '@/components/CookiePolicyContainer';
 import { networkTag } from '@/utils/common';
 import { runtimeEnv } from '@/base/env';
-import { useRouter } from 'next/router';
 
 interface HeaderProps {
   taskManagement?: boolean;
 }
 
 export const Header = memo<HeaderProps>(function Header({ taskManagement = true }) {
-  const router = useRouter()
   return (
     <>
       <HeaderContainer>
@@ -26,11 +24,6 @@ export const Header = memo<HeaderProps>(function Header({ taskManagement = true 
           {runtimeEnv === 'testnet' && <Badge>{networkTag(runtimeEnv)}</Badge>}
         </LogoContainer>
         <Content>
-          <Box onClick={() => {
-            router.push('/accounts/0x91E9CC2936498B15db93d64C2e88871F43d4C509')
-          }}>
-            detail
-          </Box>
           {taskManagement && (
             <>
               <TaskManagement />
