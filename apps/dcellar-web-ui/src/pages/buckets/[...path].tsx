972dbe8b53541117161e7d9515c667bf7ce927ac
aidencao <aiden.c@nodereal.io>
2023-11-03 15:06:38 +0800

    feat: update object list page default max widht

--- apps/dcellar-web-ui/src/pages/buckets/[...path].tsx
+++ apps/dcellar-web-ui/src/pages/buckets/[...path].tsx
@@ -1,6 +1,18 @@
 import { ObjectsPage } from '@/modules/object';
 import React from 'react';
+import { css, Global } from '@emotion/react';
 
 export default function Objects() {
-  return <ObjectsPage />;
+  return (
+    <>
+      <ObjectsPage />
+      <Global
+        styles={css`
+          #layout-main {
+            min-width: 949px;
+          }
+        `}
+      />
+    </>
+  );
 }
