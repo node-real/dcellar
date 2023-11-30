b2807edb2fa8e0429275f9a5f0c9f9e3c013004d
devinxl <devin@nodereal.io>
2023-11-30 11:33:22 +0800

    fix(dcellar-web-ui): merge arr error

--- apps/dcellar-web-ui/src/utils/dashboard/index.ts
+++ apps/dcellar-web-ui/src/utils/dashboard/index.ts
@@ -1,3 +1,4 @@
+import { BN } from '../math';
 import { formatDateUTC } from '../time'
 
 export function formatChartTime(time: string | number) {
@@ -5,6 +6,16 @@ export function formatChartTime(time: string | number) {
 }
 
 export const mergeArr = (arr1: string[], arr2: string[]) => {
-  const longArr = arr1.length >= arr2.length ? arr1 : arr2
-  return longArr.map((_, index) => String(Number(arr1[index] || 0) + Number(arr2[index] || 0)))
+  const diffLen = arr1.length - arr2.length;
+  let sameLenArr1: string[] = [];
+  let sameLenArr2: string[] = [];
+  if (diffLen > 0) {
+    sameLenArr1 = arr1;
+    sameLenArr2 = new Array(diffLen).fill(0).concat(arr2);
+  } else {
+    sameLenArr2 = arr2;
+    sameLenArr1 = new Array(diffLen).fill(0).concat(arr1);
+  }
+
+  return sameLenArr1.map((item, index) => BN(item).plus(sameLenArr2[index] || 0).toString());
 }
