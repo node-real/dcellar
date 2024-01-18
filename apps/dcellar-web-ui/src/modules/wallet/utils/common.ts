56055805bfe19b352f42cbb5f4c1e64d0821a5ce
devinxl <94832688+devinxl@users.noreply.github.com>
2024-01-18 16:49:24 +0800

    feat(dcellar-web-ui): enhance gas fee estimation for max feature on transferin with TW (#331)

--- apps/dcellar-web-ui/src/modules/wallet/utils/common.ts
+++ apps/dcellar-web-ui/src/modules/wallet/utils/common.ts
@@ -3,9 +3,13 @@ import { TFeeData, TWalletFromValues } from '../type';
 import { CRYPTOCURRENCY_DISPLAY_PRECISION } from '../constants';
 import { UseFormSetValue } from 'react-hook-form';
 
-export const setMaxAmount = (balance: string, feeData: TFeeData, setValue: UseFormSetValue<TWalletFromValues>) => {
+
+// When logging in through TW and using the "max" functionality for the "transfer in" operation, please be aware that the gas fee calculated in the TW wallet interface may be higher than the actual fee. Therefore, we have included a safety value to ensure successful transactions. If you prefer to use the max functionality with a more accurate gas fee estimation, we recommend using MetaMask.
+const TW_GAS_FEE_SAFETY_MULTIPLIER = 1.6;
+export const setMaxAmount = (balance: string, feeData: TFeeData, setValue: UseFormSetValue<TWalletFromValues>, isTwTransferMax?: boolean) => {
+  const gasFee = isTwTransferMax ? BN(feeData.gasFee).times(TW_GAS_FEE_SAFETY_MULTIPLIER) : feeData.gasFee;
   const availableBalance = BN(balance)
-    .minus(feeData.gasFee)
+    .minus(gasFee)
     .minus(feeData.relayerFee)
     .dp(CRYPTOCURRENCY_DISPLAY_PRECISION, 1)
     .toNumber();
