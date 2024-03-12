8c8a63d6962b0abd8342bf80a123331af6b26907
devinxl <devin@nodereal.io>
2024-03-12 18:01:24 +0800

    feat(dcellar-web-ui): compatible apollo env variables

--- apps/dcellar-web-ui/src/pages/api/sp-meta.ts
+++ apps/dcellar-web-ui/src/pages/api/sp-meta.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import { defaultApolloConfig, mainnetSpMetaEndpoint } from '@/base/env';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const endpoint = ((global as any).__GLOBAL_CONFIG || {}).SP_RECOMMEND_META;
-  const mainnetEndpoint = ((global as any).__GLOBAL_CONFIG || {}).MAINNET_SP_RECOMMEND_META;
+  const endpoint =
+    ((global as any).__GLOBAL_CONFIG || {}).SP_RECOMMEND_META ||
+    defaultApolloConfig.SP_RECOMMEND_META;
+  const mainnetEndpoint =
+    ((global as any).__GLOBAL_CONFIG || {}).MAINNET_SP_RECOMMEND_META || mainnetSpMetaEndpoint;
   const finalEndpoint = req.query.network === 'mainnet' ? mainnetEndpoint : endpoint;
 
   if (!finalEndpoint) res.json([]);
