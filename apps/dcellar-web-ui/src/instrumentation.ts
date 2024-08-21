393b83567a18fca83196015782c2f952fc271893
aiden-cao <104969608+aiden-cao@users.noreply.github.com>
2024-08-21 11:47:03 +0800

    feat(dcellar-web-ui): add apollo configuration tip (#393)

--- apps/dcellar-web-ui/src/instrumentation.ts
+++ apps/dcellar-web-ui/src/instrumentation.ts
@@ -5,11 +5,20 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { ConfigService } = await import('apollo-node-client');
     // https://apollo.bk.nodereal.cc/ https://apollo.nodereal.link/
+    const apolloEndpoint = `https://apollo-config.bk.nodereal.cc`;
+
+    if (process.env.NODE_ENV === 'development') {
+      console.error(
+        '\x1b[36m%s\x1b[0m',
+        `This Apollo endpoint(${apolloEndpoint}) configuration is for internal use only, please replace it with your own service endpoint, or if you do not have an Apollo configuration service, you can directly delete the instrumentation file.`,
+      );
+    }
+
     const service = new ConfigService({
       appId: 'dcellar-ui',
       configServerUrl:
         process.env.NODE_ENV === 'development'
-          ? 'https://apollo-config.bk.nodereal.cc'
+          ? apolloEndpoint
           : 'http://configcenter-apollo-configservice.configcenter.svc.cluster.local:8080',
     });
 
